dd69ccbeab21798599e8b9f6cace1ba4d251cfdb
mubarak <skinnynigg334@gmail.com>
2024-05-24 09:13:57 +0000

    initialized a new springboot application

---
+++ src/main/java/com/amalitechnss/Lizzy_fileServer/LizzyFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.amalitechnss.Lizzy_fileServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizzyFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LizzyFileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalitechnss/Lizzy_fileServer/LizzyFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitechnss.Lizzy_fileServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LizzyFileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
